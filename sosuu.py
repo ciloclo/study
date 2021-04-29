607c202dede8e17281e816dc2cf252b7cd4f75a7
ciloclo <68695670+ciloclo@users.noreply.github.com>
2021-04-29 21:49:27 +0900

    Update sosuu.py

--- sosuu.py
+++ sosuu.py
@@ -1,6 +1,6 @@
 import time
 ST=time.time()
-n=10000000
+n=10000
 q=[1]
 a=0
 for i in range(1,n+1):
@@ -10,13 +10,12 @@ for i in range(1,n+1):
                 break#1の時
             if p==1:
                 q.append(i)
+                del q[0]
                 break#2の時
             if i%p==0:
                 break
             q.append(i)
             break#3,5の時
-        if p==1:
-            continue
         if i%p==0:
             break
         #軽量化１
@@ -24,6 +23,7 @@ for i in range(1,n+1):
         if p>a:
             q.append(i)
             break
-del q[0]
 ET=time.time()
+print(q)
+print(len(q))
 print(ET-ST)
